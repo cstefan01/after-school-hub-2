cfc74ebd3df2f73ad83a80f6384789e841f23d15
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2023-12-11 22:52:06 +0000

    connecting mongodb cluster and implementing the functionality for database connectivity

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const fs = require('fs');
+const { MongoClient } = require('mongodb');
 
+
+// ================ Server Config ================ 
 const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
+// ================ Server Config ================ 
+
+// ============ Database Server Config ===========
+const DATABASE_NAME = "db_after_school_hub";
+const LESSON_COLLECTION = "lesson";
+const USERNAME = 'cstefan01';
+const PASSWORD = 'database1';
+const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.s9daasc.mongodb.net/?retryWrites=true&w=majority`;
+// ============ Database Server Config ===========
+
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDB() {
+    try {
+
+        await client.connect();
+
+        const db = client.db(DATABASE_NAME);
+        const lesson_collection = db.collection(LESSON_COLLECTION);
+
+    } catch (err) {
+        console.error('Error connecting to MongoDB Atlas', err);
+    }
+}
+
+connectToMongoDB();
+
 
 const app = express();
 
