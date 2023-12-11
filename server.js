0c4dd67b75af19c14890884b4dce2abf40698f0d
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2023-12-11 18:34:39 +0000

    implementing the logger middleware that outputs all client requests and store them within /logs/request.log

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000;
+const HOST = 'localhost';
+
+const app = express();
+
+const logger = (req, res, next) => {
+    const { method, originalUrl, protocol } = req;
+    const timestamp = new Date().toISOString();
+
+    const logMessage = `[${timestamp}] ${method} ${originalUrl} - ${protocol}://${req.get('host')}${req.originalUrl}]`;
+
+    fs.appendFile('./logs/request.log', logMessage, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+        }
+    });
+
+    console.log(logMessage);
+
+    next();
+};
+
+app.use(logger);
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is listening on http://${HOST}:${PORT}`);
+})
+
+
+
+
