9cae8e108590c891e3cd825794ad5eabe12c9ae2
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2024-01-28 18:53:01 +0000

    updating image middleware

--- middleware/image.js
+++ middleware/image.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 const getImage = (req, res, next) => {
+    // Assuming the image path is provided in the request URL
     const imagePath = path.join(__dirname, '../images', req.params.filename);
 
-    // Check if the image file exists
+    // Check if the file exists
     fs.access(imagePath, fs.constants.F_OK, (err) => {
         if (err) {
-            // If the file doesn't exist, send a 404 response
-            return res.status(404).send('Image not found');
+            // If the file does not exist, send a 404 response
+            res.status(404).send('Image not found');
+        } else {
+            // If the file exists, send the image as a response
+            res.sendFile(imagePath);
         }
-
-        // If the file exists, set the appropriate headers and send the file
-        res.setHeader('Content-Type', 'image/jpeg'); // Adjust the content type based on your image type
-        fs.createReadStream(imagePath).pipe(res);
     });
 };
 
